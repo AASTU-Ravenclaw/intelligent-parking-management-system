502fed626bb944c001b8e5a00c792c0d98366809
meronheron <merongetaneh.sel9@gmail.com>
2025-12-24 15:26:18 +0300

    fix:duplicated folder removed

--- docs/src/parking.js
+++
@@ -1,9 +0,0 @@
-function bookParking() {
-    const type = document.getElementById('vehicleType').value;
-    let floor = "";
-    if (type === "Truck") floor = "Ground Floor";
-    else if (type === "Van") floor = "Floor 1";
-    else floor = "Floor 1 or 2";
-    
-    document.getElementById('message').innerText = `Success! ${type} booked on ${floor}.`;
-}
